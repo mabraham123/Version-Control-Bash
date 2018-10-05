588c16e8ed41695358fea716fb1ec2f664e2171e
mabraham123 <33029013+mabraham123@users.noreply.github.com>
2018-10-05 14:40:46 +0100

    Validation for Main Menu+ Menu for the repository

--- repositorymenu.sh
+++ repositorymenu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-clear
+
 displayMenu()
 {
 	echo -e "--------------------------------------------------------\n"
@@ -11,41 +11,103 @@ displayMenu()
 	echo -e "--------------------------------------------------------\n"
 }
 
+ProcessMainMenu(){
+#initalise Menu
 runMenu=0
+
 while [ "$runMenu" = "0" ] #true
 do
-	echo "$runMenu"
 	displayMenu
+	#Get the users option
 	read choice
+	#Process the Users Option
 	if [ "$choice" = "1" ]
 		then
 			#Prompt name and create the directory
 			echo "Please enter the name of your new repository"
 			read repName
-			mkdir $repName
-			echo "$repName has been created"
+			#Check if the repo the user wants to create already exists
+			if [ -d $repName ]
+			then
+				#Display error message that the repo exits already
+				echo ""
+				echo -e "ERROR:  The repository: $repName already exists\n\tChoose a new name or open that repository"
+				echo ""
+			else
+				#Make the repo
+				mkdir $repName
+					echo "SUCCESS: $repName has been created"
+					echo ""
+				#Display menu for the repo
+				RepositoryMenu "$repName"
+				#End loop
+				runMenu=1
+			fi
 	elif [ "$choice" = "2" ]
 		then
 			ls -1
 			#statements
 	elif [ "$choice" = "3" ]
 		then
-			runMenu=1 #false
 			echo "The program will now exit"
-			#statements
+			exit 0
 	else
-		echo "$choice"
-		echo "error"
+		#The user entered something that was not a valid entry
+		echo ""
+		echo "ERROR:	Please enter a valid whole number from the selection above"
+		echo ""
 			#statements
 	fi 
 
 done
+}
+
+
+RepositoryMenu(){
+#Initalise varaibles
+runMenu=0
+
+while [ "$runMenu" = "0" ] #true
+do
+
+	#Display the Repo Menu
+	echo -e "--------------------------------------------------------\n"
+	echo -e "Repository Menu- $1\n"
+	echo -e "--------------------------------------------------------\n"
+	echo -e "1. Select File\n"
+	echo -e "2. Pull\n"
+	echo -e "3. Push\n"
+	echo -e "4. Exit\n"
+	echo -e "--------------------------------------------------------\n"
+
+	#Get the users option
+	read choice
+	#Process the Users Option
+	if [ "$choice" = "1" ]
+		then
+			echo "Select File Option"
+	elif [ "$choice" = "2" ]
+		then
+			echo "Pull Option"
+	elif [ "$choice" = "3" ]
+		then
+			echo "Push Option"
+			#statements
+	elif [ "$choice" = "4"]
+		then
+			runMenu=1 #false
+			echo "The program will now exit"
+	else
+		#The user entered something that was not a valid entry
+		echo ""
+		echo "ERROR:	Please enter a vaild whole number from the selection above"
+		echo ""
+	fi 
+
+done
+}
 
 
 
-
-
-
-
-
-
+#Main Method
+ProcessMainMenu
